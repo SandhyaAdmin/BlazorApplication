4748c2fb3b7a854be7c208fd7dafba119a208127
Sridhar <sri.tech46@gmail.com>
2023-05-18 00:32:38 -0500

    One way and two way data binding

---
+++ EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "Sandhya";
+        protected string Gender { get; set; } = "Female";
+        protected string Color { get; set; } = "background-color:white";
+    }
+}
