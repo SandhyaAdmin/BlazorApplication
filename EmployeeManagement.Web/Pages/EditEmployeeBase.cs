0380335444b08caf5085836f954c2a83c3fa20d2
Sridhar <sri.tech46@gmail.com>
2023-05-23 14:29:29 -0500

    Building razor form using <EditForm>

---
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public Employee Employee = new Employee();
+
+        [Parameter]
+        public string ID { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(ID));
+        }
+    }
+}
