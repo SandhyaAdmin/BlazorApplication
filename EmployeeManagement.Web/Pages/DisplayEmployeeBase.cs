654912ce1e1abbcc8fe308cef100b276aa3e32fc
Sridhar <sri.tech46@gmail.com>
2023-06-07 21:38:09 -0500

    Navigation to list after deltetion using force reload

--- EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -1,10 +1,17 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace EmployeeManagement.Web.Pages
 {
     public class DisplayEmployeeBase : ComponentBase
     {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         [Parameter]
         public Employee Employee { get; set; }
         
@@ -17,6 +24,14 @@ namespace EmployeeManagement.Web.Pages
         protected async Task CheckBoxChange(ChangeEventArgs e)
         {
             await OnEmployeeSeclection.InvokeAsync((bool)e.Value);
+        }
+        protected async Task Delete_Click()
+        {
+            await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+            /* First approach(Force reload: Full page reload): After Employeerecord gets deleted, navigate to EmployeeList.
+                The most imp thing to rememer is pass true in second parameter for force reload
+            */
+            NavigationManager.NavigateTo("/", true); 
         }
 
     }
