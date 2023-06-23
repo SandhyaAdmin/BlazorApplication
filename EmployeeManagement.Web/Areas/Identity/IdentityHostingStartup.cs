3afbaab3841948e2950368b7237aaa1ee6f65f6c
Sridhar <sri.tech46@gmail.com>
2023-06-22 19:09:06 -0500

    Identity code Set Up

---
+++ EmployeeManagement.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EmployeeManagement.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+[assembly: HostingStartup(typeof(EmployeeManagement.Web.Areas.Identity.IdentityHostingStartup))]
+namespace EmployeeManagement.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<EmployeeManagementWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EmployeeManagementWebContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 8;
+
+                    options.Lockout.MaxFailedAccessAttempts = 5;
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+
+                    options.SignIn.RequireConfirmedEmail = true;
+                })
+                .AddEntityFrameworkStores<EmployeeManagementWebContext>();
+
+            });
+        }
+    }
+}
